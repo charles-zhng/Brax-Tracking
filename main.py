0f4bcbf56203f4c47a2174bd47342e28caf4a28e
Elliott Abe <elliottabe@gmail.com>
2024-10-01 17:10:39 -0700

    fix configs

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 
 os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"] = "0.90"
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"  # Use GPU 1
+# os.environ["CUDA_VISIBLE_DEVICES"] = "1"  # Use GPU 1
 import functools
 import jax
 jax.config.update("jax_enable_x64", True)
